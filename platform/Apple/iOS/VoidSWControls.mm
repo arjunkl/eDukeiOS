f64ff1b166df99b768f0d91afc50a252b5cca92c
arjunkl <58782223+arjunkl@users.noreply.github.com>
2026-07-13 01:06:10 +0530

    Provide initial VoidSW mobile control bridge symbols

--- platform/Apple/iOS/VoidSWControls.mm
+++ platform/Apple/iOS/VoidSWControls.mm
@@ -19,3 +19,29 @@ extern "C" char *EDuke32_IOS_SelectGame(void)
 {
     return strdup("SW.GRP");
 }
+
+
+#include "../../../source/mact/include/control.h"
+
+void CONTROL_Android_ClearButton(int32_t button)
+{
+    (void)button;
+}
+
+void CONTROL_Android_PollDevices(ControlInfo *info)
+{
+    (void)info;
+}
+
+void CONTROL_Android_SetLastWeapon(int weapon)
+{
+    (void)weapon;
+}
+
+void CONTROL_Android_ScrollMap(int32_t *angle, int32_t *x, int32_t *y, uint16_t *zoom)
+{
+    (void)angle;
+    (void)x;
+    (void)y;
+    (void)zoom;
+}
